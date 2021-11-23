0e0d1cdae23b95b60a0e768f6b1022d240f5dba5
Eddie <eddielacrosse2@gmail.com>
2021-11-23 15:38:50 -0500

    added properties page and service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,6 +22,10 @@ const routes: Routes = [
   {
     path: 'contact',
     loadChildren: () => import('./pages/contact/contact.module').then( m => m.ContactPageModule)
+  },
+  {
+    path: 'properties',
+    loadChildren: () => import('./pages/properties/properties.module').then( m => m.PropertiesPageModule)
   }
 ];
 
